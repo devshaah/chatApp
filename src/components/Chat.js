4c4d2f26206ad5508de13dab09538011c8284888
devshaah <devshah0404@gmail.com>
2023-05-06 01:53:58 +0530

    first

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,7 +4,7 @@ const Chat = () => {
   return (
     <div className='chat'>
       <div className='chatInfo'>
-        <span>Jane</span>
+        <span>Jane Doe</span>
         <div className='chatIcons'>
            <img src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.similarpng.com%2Fvery-thumbnail%2F2021%2F08%2FInstagram-video-chat-icon-on-transparent-background-PNG.png&tbnid=e3lbSFUzAK50rM&vet=12ahUKEwiMxMny-t7-AhWSXHwKHVT5DqgQMygTegUIARDuAQ..i&imgrefurl=https%3A%2F%2Fsimilarpng.com%2Finstagram-video-chat-icon-on-transparent-background-png%2F&docid=qEx2lt00AE0CoM&w=600&h=600&q=video%20call%20logo&ved=2ahUKEwiMxMny-t7-AhWSXHwKHVT5DqgQMygTegUIARDuAQ'/>
            <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fthenounproject.com%2Fbrowse%2Ficons%2Fterm%2Fadd-participant%2F&psig=AOvVaw2ewhwJGJdA22iZO5lZ8NSo&ust=1683403060830000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCa9Zz73v4CFQAAAAAdAAAAABAJ'/>
